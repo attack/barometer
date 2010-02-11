e764bcd44b37114c73f3b2b0175b6028a93af793
Mark <rtec88@gmail.com>
2010-02-10 18:23:35 -0700

    debugging

--- lib/barometer/extensions/httparty.rb
+++ lib/barometer/extensions/httparty.rb
@@ -7,10 +7,11 @@ module HTTParty
     private
     
       def get_response
-        self.last_response = perform_actual_request
-        puts self.last_response.inspect
-        puts self.last_response.body.inspect
-        options[:format] ||= format_from_mimetype(last_response['content-type'])
+        response = perform_actual_request
+        puts response.inspect
+        puts response.body.inspect
+        options[:format] ||= format_from_mimetype(response['content-type'])
+        response
       end
     
   end
