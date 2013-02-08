16055294029f39471eb32a668be83d88cbd2ad64
Mark <rtec88@gmail.com>
2013-02-07 22:03:57 -0500

    start using vcr on webservice geocode

--- spec/web_services/geocode_spec.rb
+++ spec/web_services/geocode_spec.rb
@@ -1,15 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe "Query::Geocode" do
-  
+describe Barometer::WebService::Geocode do
+  use_vcr_cassette
+
   before(:each) do
     @zipcode = "90210"
   end
-  
+
   describe "and the class methods" do
-    
     describe "fetch," do
-      
       it "requires a Query object" do
         lambda { Barometer::WebService::Geocode.fetch }.should raise_error(ArgumentError)
         lambda { Barometer::WebService::Geocode.fetch("invalid") }.should raise_error(ArgumentError)
@@ -17,19 +16,16 @@ describe "Query::Geocode" do
         query.is_a?(Barometer::Query).should be_true
         lambda { Barometer::WebService::Geocode.fetch(query) }.should_not raise_error(ArgumentError)
       end
-      
+
       it "queries (key no longer required)" do
         query = Barometer::Query.new(@zipcode)
         Barometer::WebService::Geocode.fetch(query).should_not be_nil
       end
-      
+
       it "returns a Geo object" do
         query = Barometer::Query.new(@zipcode)
         Barometer::WebService::Geocode.fetch(query).is_a?(Data::Geo).should be_true
       end
-      
     end
-    
   end
-  
 end
