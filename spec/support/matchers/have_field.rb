db6e3b6fcd68cbdbe9e7b02742809529367bca9d
Mark <rtec88@gmail.com>
2013-02-18 16:53:39 -0500

    have_field matcher supports more field types

--- spec/support/matchers/have_field.rb
+++ spec/support/matchers/have_field.rb
@@ -46,24 +46,42 @@ module Barometer
       end
 
       def type_casts_as_type?
-        set_value 10
+        if type_is_a_date?
+          set_value "2013-01-01 10:15:30 am"
+        elsif type_is_a_time?
+          set_value "10:15:30 am"
+        else
+          set_value 10
+        end
         assert value.is_a?(@type), "#{@field} does not typecast as #{@type}"
       end
 
       def sets_value?
-        set_value 10
-        assert value.to_i == 10, "expected value of '10', got '#{value.to_i}'"
+        if type_is_a_date?
+          set_value "10 15 30 2013 01 01 am", "%H %M %S %Y %m %d %p"
+          assert value.to_s(true) == "2013-01-01 10:15:30 am", "expected value of '2013-01-01 10:15:30 am', got '#{value.to_s(true)}'"
+        elsif type_is_a_time?
+          set_value "10:15:30 am"
+          assert value.to_s(true) == "10:15:30 am", "expected value of '10:15:30 am', got '#{value.to_s(true)}'"
+        else
+          set_value 10
+          assert value.to_i == 10, "expected value of '10', got '#{value.to_i}'"
+        end
       end
 
       def has_correct_metric_units?
         @subject.metric = true
+        return true unless value_responds_to_metric?
         set_value 10
+
         assert value.units == metric_units, "expected units of '#{metric_units}', got '#{value.units}'"
       end
 
       def has_correct_imperial_units?
         @subject.metric = false
+        return true unless value_responds_to_metric?
         set_value 10
+
         assert value.units == imperial_units, "expected units of '#{imperial_units}', got '#{value.units}'"
       end
 
@@ -73,8 +91,12 @@ module Barometer
         @subject.send(@field)
       end
 
-      def set_value(value)
-        @subject.send("#{@field}=", value)
+      def set_value(*value)
+        if type_is_a_date? || type_is_a_time?
+          @subject.send("#{@field}=", value)
+        else
+          @subject.send("#{@field}=", *value)
+        end
       end
 
       def metric_units
@@ -87,6 +109,22 @@ module Barometer
 
       def expectation
         "\"#{@field}\" to be a #{@type}"
+      end
+
+      def value_responds_to_metric?
+        if type_is_a_date? || type_is_a_time?
+          false
+        else
+          @type.respond_to?(:new) && @type.send(:new).respond_to?(:metric)
+        end
+      end
+
+      def type_is_a_date?
+        @type == Data::LocalDateTime
+      end
+
+      def type_is_a_time?
+        @type == Data::LocalTime
       end
 
       def assert(test, failure_message)
