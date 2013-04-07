5039397f45c6c65afb98e868380e35dd89b011aa
Mark <rtec88@gmail.com>
2013-04-07 16:25:15 -0400

    remove unused code from short_zipcode format

--- lib/barometer/formats/short_zipcode.rb
+++ lib/barometer/formats/short_zipcode.rb
@@ -8,11 +8,9 @@ module Barometer
   # :short_zipcode and what the country_code is.
   #
   class Query::Format::ShortZipcode < Query::Format
-
     def self.format; :short_zipcode; end
     def self.country_code(query=nil); "US"; end
     def self.regex; /(^[0-9]{5}$)/; end
-
   end
 end
 
--- spec/formats/short_zipcode_spec.rb
+++ spec/formats/short_zipcode_spec.rb
@@ -1,47 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Barometer::Query::Format::ShortZipcode do
-  before(:each) do
-    @valid = "90210"
-    @invalid = "90210-5555"
+  it ".country_code" do
+    Barometer::Query::Format::ShortZipcode.country_code.should == "US"
+    Barometer::Query::Format::ShortZipcode.country_code("ignored").should == "US"
   end
 
-  describe "and class methods" do
-    it "returns a format" do
-      Barometer::Query::Format::ShortZipcode.format.should == :short_zipcode
+  describe ".is?" do
+    it "recognizes a valid format" do
+      Barometer::Query::Format::ShortZipcode.is?("90210").should be_true
     end
 
-    it "returns a country" do
-      Barometer::Query::Format::ShortZipcode.country_code.should == "US"
-      Barometer::Query::Format::ShortZipcode.country_code("ignored").should == "US"
-    end
-
-    it "returns a regex" do
-      Barometer::Query::Format::ShortZipcode.regex.should_not be_nil
-      Barometer::Query::Format::ShortZipcode.regex.is_a?(Regexp).should be_true
-    end
-
-    describe "is?," do
-      it "recognizes a valid format" do
-        Barometer::Query::Format::ShortZipcode.is?(@valid).should be_true
-      end
-
-      it "recognizes non-valid format" do
-        Barometer::Query::Format::ShortZipcode.is?(@invalid).should be_false
-      end
-    end
-
-    it "stubs to" do
-      Barometer::Query::Format::ShortZipcode.to.should be_nil
-    end
-
-    it "stubs convertable_formats" do
-      Barometer::Query::Format::ShortZipcode.convertable_formats.should == []
-    end
-
-    it "doesn't convert" do
-      query = Barometer::Query.new(@valid)
-      Barometer::Query::Format::ShortZipcode.converts?(query).should be_false
+    it "recognizes non-valid format" do
+      Barometer::Query::Format::ShortZipcode.is?("90210-5555").should be_false
     end
   end
 end
