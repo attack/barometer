0ad360cfa41777c63c14821d9fd5ae70d8e1eb53
Mark <rtec88@gmail.com>
2013-04-07 16:20:29 -0400

    remove unused code from geocode format

--- lib/barometer/formats/geocode.rb
+++ lib/barometer/formats/geocode.rb
@@ -9,34 +9,8 @@ module Barometer
   # :geocode, how to convert to :geocode
   #
   class Query::Format::Geocode < Query::Format
-
     def self.format; :geocode; end
-    def self.is?(query=nil); query.is_a?(String) ? true : false; end
-    def self.convertable_formats
-      [:short_zipcode, :zipcode, :coordinates, :weather_id, :icao, :woe_id]
-    end
-
-    def self.to(query)
-      unless converted_query = query.get_conversion(format)
-        case query.format
-        when :weather_id
-          converter = Barometer::Converter::FromWeatherIdToGeocode.new(query)
-          converter.call
-        when :woe_id
-          converter = Barometer::Converter::FromWoeIdToGeocode.new(query)
-          converter.call
-        else
-          converter = Barometer::Converter::ToGeocode.new(query)
-          converter.call
-        end
-      end
-    end
-
-    def self.geocode(query)
-      converter = Barometer::Converter::ToGeocode.new(query)
-      converter.call
-    end
-
+    def self.is?(query); true; end
   end
 end
 
