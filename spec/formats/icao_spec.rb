67e5e7b61dd260df3a2c167b8c68c17e2e03aebd
Mark <rtec88@gmail.com>
2013-04-07 16:22:40 -0400

    remove unused code from icao format

--- spec/formats/icao_spec.rb
+++ spec/formats/icao_spec.rb
@@ -1,55 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Barometer::Query::Format::Icao do
-  before(:each) do
-    @valid = "KSFO"
-    @invalid = "invalid"
+  it ".country_code" do
+    Barometer::Query::Format::Icao.country_code.should be_nil
+    Barometer::Query::Format::Icao.country_code("KSFO").should == "US"
+    Barometer::Query::Format::Icao.country_code("CYYC").should == "CA"
+    Barometer::Query::Format::Icao.country_code("ETAA").should == "DE"
   end
 
-  describe "and class methods" do
-    it "returns a format" do
-      Barometer::Query::Format::Icao.format.should == :icao
+  describe ".is?" do
+    it "recognizes a valid format" do
+      Barometer::Query::Format::Icao.is?("KSFO").should be_true
     end
 
-    it "returns a country" do
-      Barometer::Query::Format::Icao.country_code.should be_nil
-      Barometer::Query::Format::Icao.country_code("KSFO").should == "US"
-      Barometer::Query::Format::Icao.country_code("CYYC").should == "CA"
-      Barometer::Query::Format::Icao.country_code("ETAA").should == "DE"
-    end
-
-    it "returns a regex" do
-      Barometer::Query::Format::Icao.regex.should_not be_nil
-      Barometer::Query::Format::Icao.regex.is_a?(Regexp).should be_true
-    end
-
-    it "returns the convertable_formats" do
-      Barometer::Query::Format::Icao.convertable_formats.should_not be_nil
-      Barometer::Query::Format::Icao.convertable_formats.is_a?(Array).should be_true
-      Barometer::Query::Format::Icao.convertable_formats.should == []
-    end
-
-    describe "is?," do
-      it "recognizes a valid format" do
-        Barometer::Query::Format::Icao.is?(@valid).should be_true
-      end
-
-      it "recognizes non-valid format" do
-        Barometer::Query::Format::Icao.is?(@invalid).should be_false
-      end
-    end
-
-    it "stubs to" do
-      Barometer::Query::Format::Icao.to.should be_nil
-    end
-
-    it "stubs convertable_formats" do
-      Barometer::Query::Format::Icao.convertable_formats.should == []
-    end
-
-    it "doesn't convert" do
-      query = Barometer::Query.new(@valid)
-      Barometer::Query::Format::Icao.converts?(query).should be_false
+    it "recognizes non-valid format" do
+      Barometer::Query::Format::Icao.is?("invalid").should be_false
     end
   end
 end
