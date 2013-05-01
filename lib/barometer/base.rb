108821016320f1c5dbbc22127551a3e07f184764
Mark <rtec88@gmail.com>
2013-04-30 21:35:03 -0400

    use ConfigReader in base

--- lib/barometer/base.rb
+++ lib/barometer/base.rb
@@ -10,14 +10,13 @@ module Barometer
     def measure(metric=nil)
       @weather.start_at = Time.now.utc
 
-      level = 1
+      tier = 1
       until @weather.success?
-        if sources = Barometer.config[level]
-          _dig(sources, nil, metric)
-        else
-          raise OutOfSources
+        Utils::ConfigReader.each_service(tier) do |source, config|
+          options = { :metric => metric }.merge!(config)
+          _measure(source, options)
         end
-        level += 1
+        tier += 1
       end
 
       @weather.end_at = Time.now.utc
@@ -26,37 +25,9 @@ module Barometer
 
     private
 
-    # iterate through the setup until we have a source name (and possibly
-    # a config for that source), then measure with that source
-    #
-    # this allows for many different config formats, like
-    # { 1 => :wunderground }
-    # { 1 => [:wunderground]}
-    # { 1 => [:wunderground, :yahoo]}
-    # { 1 => [:wunderground, {:yahoo => {:weight => 2}}]}
-    # { 1 => {:wunderground => {:weight => 2}}}
-    # { 1 => [{:wunderground => {:weight => 2}}]}
-    #
-    def _dig(data, config, metric)
-      if data.is_a?(String) || data.is_a?(Symbol)
-        _measure(data, config, metric)
-      elsif data.is_a?(Array)
-        data.each do |datum|
-          _dig(datum, nil, metric)
-        end
-      elsif data.is_a?(Hash)
-        data.each do |datum, config|
-          _dig(datum, config, metric)
-        end
-      end
-    end
-
-    def _measure(source, config, metric)
-      options = { :metric => metric }
-      options.merge!(config)
-
+    def _measure(source, options)
       response = Barometer::WeatherService.measure(source.to_sym, @query, options)
-      response.weight = config[:weight] if config && config[:weight]
+      response.weight = options[:weight] if options && options[:weight]
 
       @weather.responses << response
     end
