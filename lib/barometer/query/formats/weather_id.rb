07d57f898e81830765b658252896d771cb58d7d9
Mark <rtec88@gmail.com>
2013-10-26 18:23:16 -0400

    relocate :weather_id specific code

--- lib/barometer/query/formats/weather_id.rb
+++ lib/barometer/query/formats/weather_id.rb
@@ -6,9 +6,24 @@ module Barometer
       # eg. USGA0028
       #
       class WeatherID < Base
+        @@fixes_file = File.expand_path(
+          File.join(File.dirname(__FILE__), 'translations', 'weather_country_codes.yml'))
+        @@fixes = nil
+
         def self.regex; /(^[A-Za-z]{4}[0-9]{4}$)/; end
         def self.country_code(query)
           (query && query.size >= 2) ? _fix_country(query[0..1]) : nil
+        end
+
+        private
+
+        # weather.com uses non-standard two letter country codes that
+        # hinder the ability to determine the country or fetch geo_data.
+        # correct these "mistakes"
+        #
+        def self._fix_country(country_code)
+          @@fixes ||= YAML.load_file(@@fixes_file)
+          @@fixes[country_code.upcase.to_s] || country_code
         end
       end
     end
