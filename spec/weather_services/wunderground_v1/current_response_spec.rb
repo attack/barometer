670dba67899f050ff8fc95d5093fc4eaaaf64eab
Mark <rtec88@gmail.com>
2014-01-04 23:41:57 -0500

    fix time specs to be less year dependent

--- spec/weather_services/wunderground_v1/current_response_spec.rb
+++ spec/weather_services/wunderground_v1/current_response_spec.rb
@@ -2,14 +2,15 @@ require_relative '../../spec_helper'
 
 module Barometer::WeatherService
   describe WundergroundV1::CurrentResponse do
-    it "parses the timezones correctly" do
+    it 'parses the timezones correctly' do
       payload = Barometer::Utils::Payload.new({
-        "local_time" => "May 18, 10:46 AM PDT"
+        'local_time' => 'May 18, 10:46 AM PDT'
       })
       response = WundergroundV1::CurrentResponse.new.parse(payload)
 
-      utc_observed_at = Time.utc(2013,5,18,17,46,0)
-      utc_stale_at = Time.utc(2013,5,18,18,0,0)
+      current_year = Time.now.year
+      utc_observed_at = Time.utc(current_year,5,18,17,46,0)
+      utc_stale_at = Time.utc(current_year,5,18,18,0,0)
 
       expect( response.current.observed_at.utc ).to eq utc_observed_at
       expect( response.current.stale_at.utc ).to eq utc_stale_at
--- spec/weather_services/wunderground_v1/forecast_response_spec.rb
+++ spec/weather_services/wunderground_v1/forecast_response_spec.rb
@@ -2,56 +2,57 @@ require_relative '../../spec_helper'
 
 module Barometer::WeatherService
   describe WundergroundV1::ForecastResponse do
+    let(:current_year) { Time.now.year }
     let(:current_response) { Barometer::Response.new }
 
-    it "parses the timezones correctly" do
+    it 'parses the timezones correctly' do
       payload = Barometer::Utils::Payload.new({
-        "simpleforecast" => { "forecastday" => [
+        'simpleforecast' => { 'forecastday' => [
           {
-            "date" => {
-              "tz_long" => 'America/Los_Angeles',
-              "pretty" => '10:46 PM PDT on May 18, 2013'
+            'date' => {
+              'tz_long' => 'America/Los_Angeles',
+              'pretty' => "10:46 PM PDT on May 18, #{current_year}"
             }
           }
         ]}
       })
       response = WundergroundV1::ForecastResponse.new(current_response).parse(payload)
 
-      utc_starts_at = Time.utc(2013,5,19,5,46,0)
-      utc_ends_at = Time.utc(2013,5,20,5,45,59)
+      utc_starts_at = Time.utc(current_year,5,19,5,46,0)
+      utc_ends_at = Time.utc(current_year,5,20,5,45,59)
 
       expect( response.forecast[0].starts_at.utc ).to eq utc_starts_at
       expect( response.forecast[0].ends_at.utc ).to eq utc_ends_at
       expect( response.timezone.to_s ).to eq 'America/Los_Angeles'
     end
 
-    it "parses sun timezones correctly" do
+    it 'parses sun timezones correctly' do
       current_response.current = Barometer::Response::Current.new
-      current_response.current.observed_at = Barometer::Utils::Time.parse("May 18, 10:46 AM PDT")
+      current_response.current.observed_at = Barometer::Utils::Time.parse('May 18, 10:46 AM PDT')
       payload = Barometer::Utils::Payload.new({
-        "simpleforecast" => { "forecastday" => [
+        'simpleforecast' => { 'forecastday' => [
           {
-            "date" => {
-              "tz_long" => 'America/Los_Angeles',
-              "pretty" => '10:46 PM PDT on May 18, 2013'
+            'date' => {
+              'tz_long' => 'America/Los_Angeles',
+              'pretty' => "10:46 PM PDT on May 18, #{current_year}"
             }
           }
         ]},
-          "moon_phase" => {
-          "sunrise" => {
-            "hour" => '7', "minute" => '59'
+          'moon_phase' => {
+          'sunrise' => {
+            'hour' => '7', 'minute' => '59'
           },
-          "sunset" => {
-            "hour" => '17', "minute" => '42'
+          'sunset' => {
+            'hour' => '17', 'minute' => '42'
           }
         }
       })
       response = WundergroundV1::ForecastResponse.new(current_response).parse(payload)
 
-      utc_current_sun_rise = Time.utc(2013,5,18,14,59,0)
-      utc_current_sun_set = Time.utc(2013,5,19,0,42,0)
-      utc_forecast_sun_rise = Time.utc(2013,5,19,14,59,0)
-      utc_forecast_sun_set = Time.utc(2013,5,20,0,42,0)
+      utc_current_sun_rise = Time.utc(current_year,5,18,14,59,0)
+      utc_current_sun_set = Time.utc(current_year,5,19,0,42,0)
+      utc_forecast_sun_rise = Time.utc(current_year,5,19,14,59,0)
+      utc_forecast_sun_set = Time.utc(current_year,5,20,0,42,0)
 
       expect( response.current.sun.rise.utc ).to eq utc_current_sun_rise
       expect( response.current.sun.set.utc ).to eq utc_current_sun_set
