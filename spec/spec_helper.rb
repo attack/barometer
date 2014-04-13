8dcade2a3cde39dcb20b17b8b48ab2282895fa98
Mark Gangl <mark@attackcorp.com>
2014-04-13 14:56:02 -0400

    remove old forecast.io spec setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,15 +10,10 @@ require 'barometer/support'
 
 require_relative '../lib/barometer'
 
-FORECAST_IO_APIKEY = Barometer::Support::KeyFileParser.find(:forecast_io, :apikey) || 'forecastio'
-
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.default_cassette_options = { record: :none, serialize_with: :json }
-
-  config.filter_sensitive_data('FORECAST_IO_APIKEY') { FORECAST_IO_APIKEY.to_s }
-
   config.configure_rspec_metadata!
 end
 
